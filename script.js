eef3cf4de15a7fbec3214a20809dcb9f01fa9ed1
experiment1experiment2 <136436794+experiment1experiment2@users.noreply.github.com>
2024-01-05 15:24:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,1 @@
-function showDetails(birdName) {
-    const detailsContainer = document.getElementById('bird-details');
-    detailsContainer.innerHTML = `<h2>${birdName} Details</h2><p>Additional information about the ${birdName}.</p>`;
-}
+
