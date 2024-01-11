06d79f0447cc1bd529e8537e340b9ef82398d495
experiment1experiment2 <136436794+experiment1experiment2@users.noreply.github.com>
2024-01-11 12:53:32 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,12 @@
 let login = document.querySelector(".login-form");
+let navbar = document.querySelector(".header .navbar");
 
-document.querySelector("#login-btn").onclick = () =>{
+document.querySelector("#login-btn").onclick = () => {
     login.classList.toggle('active');
     navbar.classList.remove('active');
 }
 
-let navbar = document.querySelector(".header .navbar");
-
-document.querySelector('#menu-btn').onclick = () =>{
+document.querySelector('#menu-btn').onclick = () => {
     login.classList.remove('active');
     navbar.classList.toggle('active');
 }
